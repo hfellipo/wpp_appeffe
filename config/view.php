b2ea8018f8ad585001bfb15e39fe18dcfa0e2494
Hermê Fellipo Bordoni Caldeira <57048684+hfellipo@users.noreply.github.com>
2026-02-02 17:18:29 -0300

    update

--- config/view.php
+++ config/view.php
@@ -30,7 +30,8 @@ return [
 
     'compiled' => env(
         'VIEW_COMPILED_PATH',
-        realpath(storage_path('framework/views'))
+        // On fresh servers the directory may not exist yet; avoid returning false.
+        realpath(storage_path('framework/views')) ?: storage_path('framework/views')
     ),
 
 ];
